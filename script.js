c3071844d4bb747df4ec0089c123ca51d8cae0df
Hites Chitalia <hites27@gmail.com>
2021-01-27 16:34:32 +0530

    Added player switch functionality

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 //Selecting elements
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -17,7 +19,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice
 
@@ -32,8 +36,16 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add to current score
     currentScore += dice;
-    current0El.textContent = currentScore; //CHANGE LATER
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
   } else {
     //Switch player
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
